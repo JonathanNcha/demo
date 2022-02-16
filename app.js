661f482c582fa46973c057359beacd6eb73e990c
JonathanNcha <93585577+JonathanNcha@users.noreply.github.com>
2022-02-16 12:11:39 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.set('port', 3000);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
+
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+MongoClient.connect('mongodb+srv://root:root@cluster0.vd09u.mongodb.net'
+    , (err, client) => {
+        db = client.db('Webstore');
+    })
+
+app.get('/', (req, res, next) => {
+    res.send('Select a collection');
+})
+
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    })
+})
+
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
+
+const ObjectID = require('mongodb').ObjectID;
+
+app.get('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+        if (e) return next(e)
+        res.send(result)
+    })
+})
+
+
+//update an object 
+
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.update(
+        { _id: new ObjectID(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e) return next(e)
+            res.send(result.modifiedCount === 1 ? { msg: 'success' } : { msg: 'error' })
+        })
+})
+
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.deleteOne(
+        { _id: ObjectID(req.params.id) }, (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+                { msg: 'success' } : { msg: 'error' })
+        })
+})
+
+app.put('/collection/:collectionName/:id/reduce/:name/:value', (req, res, next) => {
+
+    let value = -1 * parseInt(req.params.value);
+    let name = req.params.name;
+
+    const attr = {};
+    attr[name] = value;
+
+    req.collection.updateOne(
+        { _id: new ObjectID(req.params.id) },
+        { "$inc": attr },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e || result.result.n !== 1) return next();
+            res.json({ message: 'success' });
+        });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+
