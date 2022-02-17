5a65693242b9f29ab6fe230914cb8754021392f5
JonathanNcha <93585577+JonathanNcha@users.noreply.github.com>
2022-02-17 18:44:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 app.set('port', 3000);
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers",'Origin,X-requested-With,Content-Type,Accept');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "*");
     next();
